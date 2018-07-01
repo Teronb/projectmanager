87c1cf2dc3930fb2197c39d89b6626921e2a70d2
Teronb <teron_b@hotmail.com>
2018-06-30 23:45:17 -0400

    Created a constructor that held a few states and super();

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+constrctor(){
+  super();
+  this.state = {
+    projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'Ecommerce Shopping Cart',
+        category: 'Web Development'
+      }
+    ]
+  }
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="App-title">Welcome to React</h2>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
